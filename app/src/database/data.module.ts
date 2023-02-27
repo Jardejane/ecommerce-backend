a12650d90db6e79e9015d7a442ceea6b7918c348
Jardejane <janefeitosalima49@gmail.com>
2023-02-26 21:59:36 -0300

    refactory code

--- app/src/database/data.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import {baseProviders} from './dados.providers'
-
-@Module({
-    providers: [...baseProviders],
-    exports: [...baseProviders]
-})
-
-export class dataModule{}
