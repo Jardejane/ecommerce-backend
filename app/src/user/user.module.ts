e5664757042070d0a4d46fe687baecc7ebe0b922
Jardejane <janefeitosalima49@gmail.com>
2023-02-28 00:23:31 -0300

    create the class addUser with findUser

--- app/src/user/user.module.ts
+++ app/src/user/user.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from '../utils/schemas/user.schema';
 import { UserService } from './user.service';
 
 @Module({
-  providers: [UserService]
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])
+  ],
+  providers: [UserService],
+  exports: [UserService]
 })
 export class UserModule {}
