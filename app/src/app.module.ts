1a52e1c591292652da14439396603ac209edd0a2
Jardejane <janefeitosalima49@gmail.com>
2023-02-28 00:30:37 -0300

    create the module Auth

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,9 +2,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb+srv://jane:1234@cluster1.6mnmyes.mongodb.net/test'),ProductModule ],
+  imports: [ MongooseModule.forRoot('mongodb+srv://jane:1234@cluster1.6mnmyes.mongodb.net/test'),ProductModule, AuthModule ],
   controllers: [],
   providers: [],
 })
